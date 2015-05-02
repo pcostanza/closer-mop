aac8486a24d43b71f3741817600c5109876ad908
Pascal Costanza <pc@p-cos.net>
2015-05-02 17:26:30 +0200

    Checked against ABCL 1.3.2 and SBCL 1.2.11 - no changes.

--- features.lisp
+++ features.lisp
@@ -1,4 +1,4 @@
-:abcl1.3.1
+:abcl1.3.2
 #| all features implemented |#
 
 :allegro8.2-9.0
@@ -282,7 +282,7 @@
  (:subclasses-of-standard-slot-definition-do-not-inherit-exported-slots)
  (:subclasses-of-standard-writer-method-do-not-inherit-exported-slots))
 
-:sbcl1.2.10
+:sbcl1.2.11
 #| all features implemented |#
 
 :scl1.3.9
