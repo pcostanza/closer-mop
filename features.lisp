5761078d08633716298527231e38d25dad51b643
Pascal Costanza <pc@p-cos.net>
2020-12-19 15:57:31 +0100

    Checked against Clozure Common Lisp 1.12 - no changes.

--- features.lisp
+++ features.lisp
@@ -83,7 +83,7 @@
  (:remove-method-calls-compute-discriminating-function)
  (:subclasses-of-method-combination-do-not-inherit-exported-slots))
 
-:clozure-common-lisp1.11.5
+:clozure-common-lisp1.12
 ((:add-method-calls-compute-discriminating-function fixed)
  (:compute-slots-requested-slot-order-honoured)
  (:defmethod-calls-generic-function-method-class fixed)
