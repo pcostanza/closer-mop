6113654771316d20fb2f50a2c6637f734a2db925
Pascal Costanza <pc@p-cos.net>
2020-12-31 19:49:37 +0100

    Checked against SBCL 2.1.0 - no changes.

--- features.lisp
+++ features.lisp
@@ -316,7 +316,7 @@
  (:subclasses-of-standard-slot-definition-do-not-inherit-exported-slots)
  (:subclasses-of-standard-writer-method-do-not-inherit-exported-slots))
 
-:sbcl2.0.11
+:sbcl2.1.0
 #| all features implemented |#
 
 :scl1.3.9
