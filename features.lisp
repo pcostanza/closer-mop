b09347c5ccd36f60864a19c0101f0fb85317da2e
Pascal Costanza <pc@p-cos.net>
2021-02-01 18:29:07 +0100

    Checked against ECL 21.2.1 - no changes.

--- features.lisp
+++ features.lisp
@@ -159,7 +159,7 @@
  (:subclasses-of-standard-slot-definition-do-not-inherit-exported-slots)
  (:subclasses-of-standard-writer-method-do-not-inherit-exported-slots))
 
-:ecl16.1.3
+:ecl21.2.1
 ((:class-initialization-calls-reader-method-class)
  (:class-initialization-calls-writer-method-class)
  (:subclasses-of-built-in-class-do-not-inherit-exported-slots fixed)
